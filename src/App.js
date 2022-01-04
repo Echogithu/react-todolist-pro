34d13846e61ba0aba3a09f9bbf4b4503e0e2ecc3
Zeping.Cai <zeping.cai@raysightmed.com>
2022-01-04 18:47:53 +0800

    显示列表项

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import MyHeader from "./components/Header";
 import AddInput from "./components/AddInput";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
+import TodoItem from "./components/TodoItem";
 function App() {
   const [isInputShow, setIsInputShow] = useState(false),
     [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    const todoData = JSON.parse(localStorage.getItem("todoList"));
+    setTodoList(todoData);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }, [todoList])
 
   const addItem = useCallback((value) => {
     const dataItem = {
@@ -20,6 +30,11 @@ function App() {
     <div className="App">
       <MyHeader openInput={() => setIsInputShow(!isInputShow)} />
       <AddInput isInputShow={isInputShow} addItem={addItem} />
+      <ul className="todo-list">
+        {todoList.map((item, index) => {
+          return <TodoItem data={item} key={index} />;
+        })}
+      </ul>
     </div>
   );
 }
